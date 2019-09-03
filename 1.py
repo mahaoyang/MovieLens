5c9440621e8d22ec4eac97078152d854c302f90c
mahaoyang <mahydxygitlab>
2019-09-03 11:32:03 +0800

    initial

--- 1.py
+++ 1.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
-from surprise import SVDpp
-from sklearn.random_projection import sparse_random_matrix
+import pandas as pd
+from surprise import SVDpp, Dataset, Reader, NormalPredictor
+from surprise.model_selection import cross_validate
 
-X = sparse_random_matrix(10000, 10000, density=0.01, random_state=42)
-print(X)
-svd = SVDpp(n_components=2, n_iter=7, random_state=42)
-X = svd.fit_transform(X)
+ratings = pd.read_csv('data/ratings.dat', sep='::', names=['user_id', 'movie_id', 'rating', 'timestamp'])
+ratings = ratings[['user_id', 'movie_id', 'rating']]
+reader = Reader(line_format='user item rating', sep='::')
+data = Dataset.load_from_df(ratings, reader=reader)
+data = cross_validate(NormalPredictor(), data, cv=2)
+
+svd = SVDpp(n_factors=2, n_epochs=2)
+X = svd.fit(data)
 print(X)
 print(X.shape)
-print(svd.singular_values_)
