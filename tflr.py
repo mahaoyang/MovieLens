60002acd23e225617541171e477c4f60ed60ebbf
99263 <992635910@qq.com>
2020-03-11 10:38:24 +0800

    init

--- tflr.py
+++ tflr.py
@@ -2,6 +2,7 @@
 # -*- encoding: utf-8 -*-
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, mean_squared_error, mean_absolute_error
@@ -41,4 +42,41 @@ model.compile(optimizer='rmsprop',
 data = ratings[['movie_id', 'user_id', 'rating']][-200:].values
 labels = ratings['rating'].map(lambda x: 1 if int(x) > 2 else 0)[-200:].values
 model.fit(data, to_categorical(labels), epochs=10, batch_size=500)  # starts training
-print(model.predict(data[-10:]))
+print(data[-10:].tolist())
+print(model.predict(data[-10:]).tolist())
+
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    from tensorflow.python.framework.graph_util import convert_variables_to_constants
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = convert_variables_to_constants(session, input_graph_def,
+                                                      output_names, freeze_var_names)
+        return frozen_graph
+
+# save keras model as tf pb files ===============
+from keras import backend as K
+wkdir = 'C:/Users/99263/PycharmProjects/MovieLens'
+pb_filename = 'lr.pb'
+frozen_graph = freeze_session(K.get_session(),
+                              output_names=[out.op.name for out in model.outputs])
+tf.train.write_graph(frozen_graph, wkdir, pb_filename, as_text=False)
